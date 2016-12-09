a39a161a7b938927be7790f79f77e8ccc38da379
VictorBraz <victormachadobraz@hotmail.com>
2016-12-09 12:26:35 +0100

    Client erbij gezet

---
+++ App/public_html/app/controllers/HomeController.js
@@ -0,0 +1,15 @@
+
+angular.module('workshop').controller('HomeController', function($scope, authenticationService, userService)
+{
+    $scope.login = function()
+    {
+        authenticationService.createAuthentication($scope.email, $scope.password);
+        
+        userService.authenticate(function(authenticator)
+        {
+            authenticationService.setAuthenticator(authenticator);
+            authenticationService.storeAuthentication($scope.remember);
+        });
+        
+    };
+});
