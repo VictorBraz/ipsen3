a2b709ab91248cc80e064fdeb99a67de59cdc887
VictorBraz <victormachadobraz@hotmail.com>
2016-12-09 13:50:33 +0100

    DatabaseDAO en service aangemaakt, nog niet getest

---
+++ App/Server/Persistence/DatabaseDAO.java
@@ -0,0 +1,19 @@
+package Server.Persistence;
+
+import Server.Service.DatabaseService;
+
+import javax.inject.Singleton;
+import java.sql.Connection;
+
+/**
+ * Created by Victor on 9-12-2016.
+ */
+public abstract class DatabaseDAO {
+
+    @Singleton
+    protected Connection conn;
+
+    public DatabaseDAO() throws Exception{
+        this.conn = DatabaseService.getInstance().getConnection("postgres", "1");
+    }
+}
