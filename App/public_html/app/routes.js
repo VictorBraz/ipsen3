d699f4e8c23e3b113d6a58b0bcf4a8f995ddd0af
Negin Nafissi <Negin Nafissi>
2016-12-20 14:48:45 +0100

    Bezig met mederwerkers Toevoegen

--- App/public_html/app/routes.js
+++ App/public_html/app/routes.js
@@ -9,6 +9,7 @@ angular.module('workshop').config(function($routeProvider)
     $routeProvider.when('/company', {templateUrl: 'assets/partials/companydetail.html', controller: 'CompanyController' });
     $routeProvider.when('/addclient', {templateUrl: 'assets/partials/addclient.html', controller: 'ClientController' });
     $routeProvider.when('/employees', {templateUrl: 'assets/partials/employees.html', controller: 'EmployeesController' });
+    $routeProvider.when('/addEmployee', {templateUrl: 'assets/partials/addEmployee.html', controller: 'EmployeesController' });
 
     // Voeg hier meer routes toe
     
